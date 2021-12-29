67c66c042acda6c327f7d2aa080fc619e9250148
Josh Robson Chase <josh@robsonchase.com>
2021-12-29 13:16:03 -0500

    Add droid to ci

---
+++ host/cachix/nix-on-droid.nix
@@ -0,0 +1,10 @@
+{
+  nix = {
+    binaryCaches = [
+      "https://nix-on-droid.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU="
+    ];
+  };
+}
