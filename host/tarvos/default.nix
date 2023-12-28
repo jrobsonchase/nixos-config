2d61f2fa7e03e340f9cc7c052d7f8b8a844d4e65
Josh Robson Chase <josh@robsonchase.com>
2023-12-28 11:48:17 -0500

    rename nssmdns option

--- host/tarvos/default.nix
+++ host/tarvos/default.nix
@@ -103,7 +103,7 @@
   services.avahi.enable = true;
   # Important to resolve .local domains of printers, otherwise you get an error
   # like  "Impossible to connect to XXX.local: Name or service not known"
-  services.avahi.nssmdns = true;
+  services.avahi.nssmdns4 = true;
 
   services.fwupd.enable = true;
 
