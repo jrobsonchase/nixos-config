85590da2427061364117d7aca8cd651bcc74e58b
Josh Robson Chase <josh@robsonchase.com>
2024-01-15 20:45:25 -0500

    add hydra waiter

---
+++ .hydra/await_jobset.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+set -o pipefail
+
+URL=$1
+PROJECT=$2
+JOBSET=$3
+REV=$4
+
+function find_eval() {
+	curl -s -H "Accept: application/json" $URL/jobset/$PROJECT/$JOBSET/evals | \
+		jq -er "[.evals[] | select(.flake | test(\"$REV\"))] | sort_by(-.timestamp) | .[0]"
+}
+
+echo Looking for eval...
+
+while ! EVAL=$(find_eval); do
+	sleep 5
+done
+
+echo Waiting on builds...
+
+BUILDS=$(echo $EVAL | jq -r .builds[])
+
+for b in $BUILDS; do
+	DATA=$(curl -s -H "Accept: application/json" $URL/build/$b)
+	if [ $(echo "${DATA}" | jq .finished) -ne 1 ]; then
+		sleep 5
+		continue
+	fi
+	if [ $(echo "${DATA}" | jq .buildstatus) -ne 0 ]; then
+		echo Nonzero build status: $URL/build/$b
+		exit 1
+	fi
+done
+
+echo Done!
