10f00fa0efabd3c978b6e0d1cede538b7e93c40f
Josh Robson Chase <josh@robsonchase.com>
2024-02-03 06:50:44 -0500

    give jj its own directory and add templates

--- user/josh/jujutsu/default.nix
+++ user/josh/jujutsu/default.nix
@@ -4,10 +4,13 @@
     enable = true;
     enableBashIntegration = true;
     settings = {
+      ui = {
+        default-command = "log";
+      };
       user = {
         name = "Josh Robson Chase";
         email = "josh@robsonchase.com";
       };
-    };
+    } // (import ./templates.nix);
   };
 }
