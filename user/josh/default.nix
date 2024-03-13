0138361ceeee9ab849749b99f1a9766a7dde3bdd
Josh Robson Chase <josh@robsonchase.com>
2024-03-13 18:08:03 -0400

    disable sweethome3d

--- user/josh/default.nix
+++ user/josh/default.nix
@@ -40,15 +40,15 @@
     mtr
     screen
 
-    (symlinkJoin {
-      name = "sweethome3d";
-      paths = [ sweethome3d.application ];
-      buildInputs = [ makeWrapper ];
-      postBuild = ''
-        wrapProgram $out/bin/sweethome3d \
-          --prefix JAVA_TOOL_OPTIONS ' ' '-Dcom.eteks.sweethome3d.j3d.useOffScreen3DView=true'
-      '';
-    })
+    # (symlinkJoin {
+    #   name = "sweethome3d";
+    #   paths = [ sweethome3d.application ];
+    #   buildInputs = [ makeWrapper ];
+    #   postBuild = ''
+    #     wrapProgram $out/bin/sweethome3d \
+    #       --prefix JAVA_TOOL_OPTIONS ' ' '-Dcom.eteks.sweethome3d.j3d.useOffScreen3DView=true'
+    #   '';
+    # })
     geeqie
     slack
     discord
