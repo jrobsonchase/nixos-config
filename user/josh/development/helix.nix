38da8e6249a9219aa5b2ef27c5fb8b965f5f238b
Josh Robson Chase <josh@robsonchase.com>
2024-04-08 10:52:32 -0400

    helix: override monokai theme

--- user/josh/development/helix.nix
+++ user/josh/development/helix.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }:
+let
+  writeTOML = (pkgs.formats.toml { }).generate;
+  readTOML = path: (fromTOML (builtins.readFile path));
+  overrideTheme = name: overrides: writeTOML "${name}.toml" ((readTOML "${pkgs.helix.src}/runtime/themes/${name}.toml") // overrides);
+in
 {
+  xdg.configFile."helix/themes/monokai.toml".source = overrideTheme "monokai" {
+    "ui.virtual.inlay-hint".fg = "#88846F";
+  };
   programs.helix = {
     enable = true;
     settings = {
