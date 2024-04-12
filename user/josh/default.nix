c0670b399347017c5a0c87de4b5a60691d6a0c6f
Josh Robson Chase <josh@robsonchase.com>
2024-04-12 10:05:16 -0400

    trim down josh and development defaults

--- user/josh/default.nix
+++ user/josh/default.nix
@@ -2,22 +2,13 @@
 {
   imports = [
     ../common.nix
-    ./desktop
     ./gpg.nix
     ./git.nix
     ./jujutsu
-    ./desktop/firefox.nix
     ./development
 
     inputModules.private.default
-    inputModules.ngrok.ngrok
   ];
-
-  xsession.windowManager.i3.enable = true;
-
-  programs.ngrok = {
-    enable = true;
-  };
 
   programs.direnv = {
     enable = true;
@@ -25,9 +16,6 @@
   };
 
   home.packages = with pkgs; [
-    wireshark
-    nmap
-
     jq
     tree
     bc
@@ -41,24 +29,5 @@
     wget
     mtr
     screen
-
-    # (symlinkJoin {
-    #   name = "sweethome3d";
-    #   paths = [ sweethome3d.application ];
-    #   buildInputs = [ makeWrapper ];
-    #   postBuild = ''
-    #     wrapProgram $out/bin/sweethome3d \
-    #       --prefix JAVA_TOOL_OPTIONS ' ' '-Dcom.eteks.sweethome3d.j3d.useOffScreen3DView=true'
-    #   '';
-    # })
-    slack
-    discord
-    spotify
-    evince
-
-
-    appimage-run
-
-    zoom-us
   ];
 }
--- user/josh/development/default.nix
+++ user/josh/development/default.nix
@@ -1,24 +1,11 @@
 { config, lib, pkgs, ... }:
 {
   imports = [
-    ./rust.nix
-    ./rune.nix
-    ./go.nix
-    ./dotnet.nix
     ./nix.nix
-    ./vscode.nix
-    ./c_cpp.nix
-    ./sql.nix
-    ./lua.nix
-    ./kubernetes.nix
-    ./deno.nix
-    ./bazel.nix
     ./helix.nix
-    # ./clojure.nix
   ];
 
   home.packages = with pkgs; [
-    helix
     ripgrep
     vim_configurable
   ];
