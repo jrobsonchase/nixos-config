91cae8a0186d15bef3b341bf860ba7b6e10f6373
Josh Robson Chase <josh@robsonchase.com>
2024-04-12 10:03:56 -0400

    create spin for full i3, no default changes yet

---
+++ user/josh/i3-full.nix
@@ -0,0 +1,64 @@
+{ config, lib, pkgs, inputModules, ... }:
+{
+  imports = [
+    ../common.nix
+    ./desktop
+    ./gpg.nix
+    ./git.nix
+    ./jujutsu
+    ./desktop/firefox.nix
+    ./development
+
+    inputModules.private.default
+    inputModules.ngrok.ngrok
+  ];
+
+  xsession.windowManager.i3.enable = true;
+
+  programs.ngrok = {
+    enable = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
+  home.packages = with pkgs; [
+    wireshark
+    nmap
+
+    jq
+    tree
+    bc
+    file
+    usbutils
+    inetutils
+    htop
+    zip
+    unzip
+    dnsutils
+    wget
+    mtr
+    screen
+
+    # (symlinkJoin {
+    #   name = "sweethome3d";
+    #   paths = [ sweethome3d.application ];
+    #   buildInputs = [ makeWrapper ];
+    #   postBuild = ''
+    #     wrapProgram $out/bin/sweethome3d \
+    #       --prefix JAVA_TOOL_OPTIONS ' ' '-Dcom.eteks.sweethome3d.j3d.useOffScreen3DView=true'
+    #   '';
+    # })
+    slack
+    discord
+    spotify
+    evince
+
+
+    appimage-run
+
+    zoom-us
+  ];
+}
