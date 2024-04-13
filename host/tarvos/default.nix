b7f5f9782851c44bc6c72a763493122126885184
Josh Robson Chase <josh@robsonchase.com>
2024-04-13 07:47:42 -0400

    update displayManager config for nixpkgs changes

--- host/tarvos/default.nix
+++ host/tarvos/default.nix
@@ -80,13 +80,14 @@
   services.xserver = {
     enable = true;
     windowManager.i3.enable = true;
-    displayManager = {
-      defaultSession = "none+i3";
+  };
 
-      autoLogin = {
-        enable = true;
-        user = "josh";
-      };
+  services.displayManager = {
+    defaultSession = "none+i3";
+
+    autoLogin = {
+      enable = true;
+      user = "josh";
     };
   };
 
