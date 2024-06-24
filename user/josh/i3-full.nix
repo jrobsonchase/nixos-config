ae24a5d9f35c5f864ebc52ccd0519e33b6f27fd9
Josh Robson Chase <josh@robsonchase.com>
2024-06-24 11:15:31 -0400

    move sweethome3d to its own module

--- user/josh/i3-full.nix
+++ user/josh/i3-full.nix
@@ -23,15 +23,6 @@
     wireshark
     nmap
 
-    # (symlinkJoin {
-    #   name = "sweethome3d";
-    #   paths = [ sweethome3d.application ];
-    #   buildInputs = [ makeWrapper ];
-    #   postBuild = ''
-    #     wrapProgram $out/bin/sweethome3d \
-    #       --prefix JAVA_TOOL_OPTIONS ' ' '-Dcom.eteks.sweethome3d.j3d.useOffScreen3DView=true'
-    #   '';
-    # })
     slack
     discord
     spotify
