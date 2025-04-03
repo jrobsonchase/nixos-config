7bd99a7f4fd844d6e0beda8c63881aad7f54ac9d
Josh Robson Chase <josh@robsonchase.com>
2025-04-03 09:56:58 -0400

    tarvos -> ymir

---
+++ user/josh@ymir/default.nix
@@ -0,0 +1,24 @@
+{ pkgs, lib, config, inputModules, ... }:
+{
+  imports = [
+    ../common.nix
+    ../josh/development
+    ../josh/gpg.nix
+    ../josh/git.nix
+    ../josh/jujutsu
+    ../josh/zellij.nix
+
+    inputModules.private.default
+  ];
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
+  home.sessionPath = [
+    "${config.home.homeDirectory}/.cargo/bin"
+  ];
+
+  # programs.alacritty.settings.font.size = lib.mkForce 14;
+}
