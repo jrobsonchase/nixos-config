90253a9420711becdac73de491530d482f0317ac
Josh Robson Chase <josh@robsonchase.com>
2025-04-29 10:48:18 -0400

    start adding doom emacs

--- user/josh@ymir/default.nix
+++ user/josh@ymir/default.nix
@@ -16,9 +16,30 @@
     nix-direnv.enable = true;
   };
 
+  home.packages = [
+    pkgs.devenv
+  ];
+
   home.sessionPath = [
     "${config.home.homeDirectory}/.cargo/bin"
   ];
+
+  programs.emacs = {
+    enable = true;
+    extraPackages = epkgs: [ epkgs.vterm ];
+  };
+
+  home.activation = {
+    installDoom = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      [ ! -d $HOME/.config/emacs ] && \
+        run git clone --depth 1 https://github.com/doomemacs/doomemacs $HOME/.config/emacs && \
+        run $HOME/.config/emacs/bin/doom install --env --install --force
+    '';
+    syncDoom = lib.hm.dag.entryAfter [ "installDoom" ] ''
+      run $HOME/.config/emacs/bin/doom sync
+    '';
+  };
+
 
   # programs.alacritty.settings.font.size = lib.mkForce 14;
 }
