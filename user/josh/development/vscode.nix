79b63500dddeb922dcf4ce56bf74b425a0e1bf8f
Josh Robson Chase <josh@robsonchase.com>
2025-07-03 04:09:38 -0400

    fix vscode deprecation

--- user/josh/development/vscode.nix
+++ user/josh/development/vscode.nix
@@ -6,7 +6,7 @@
   ];
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       bierner.markdown-mermaid
       esbenp.prettier-vscode
       gregoire.dance
