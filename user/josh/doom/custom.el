913b8b38398a5771a4f8ba26f87b7c3787c55fe0
Josh Robson Chase <josh@robsonchase.com>
2025-07-11 12:00:59 -0400

    eshell configuration

--- user/josh/doom/custom.el
+++ user/josh/doom/custom.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("c7fd1708e08544d1df2cba59b85bd25263180b19b287489d4f17b9118487e718" default)))
+   '("c7fd1708e08544d1df2cba59b85bd25263180b19b287489d4f17b9118487e718" default))
+ '(ignored-local-variable-values '((eval progn (pp-buffer) (indent-buffer)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
