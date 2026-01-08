b73b488bb81dca90f67dbc68adfc2c6c4efa4711
Josh Robson Chase <josh@robsonchase.com>
2026-01-08 10:53:41 -0500

    fix up caches, add zed cache

--- host/caches.nix
+++ host/caches.nix
@@ -1,8 +1,8 @@
 # WARN: this file will get overwritten by $ cachix use <name>
-{ pkgs, lib, ... }:
+{ lib, ... }:
 
 let
-  folder = ./cachix;
+  folder = ./caches;
   toImport = name: value: folder + ("/" + name);
   filterCaches = key: value: value == "regular" && lib.hasSuffix ".nix" key;
   imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
--- host/common-desktop.nix
+++ host/common-desktop.nix
@@ -10,7 +10,7 @@
 }:
 {
   imports = [
-    ./cachix.nix
+    ./caches.nix
     inputModules.determinate.default
   ];
 
