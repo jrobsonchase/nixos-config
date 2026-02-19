2198cd124ef29b5ade22830805f261a051af1441
Josh Robson Chase <josh@robsonchase.com>
2026-02-19 08:40:42 -0500

    sort zed settings

--- user/josh/development/zed.nix
+++ user/josh/development/zed.nix
@@ -18,71 +18,40 @@
     ];
 
     userSettings = {
-      seed_search_query_from_cursor = "selection";
-      use_smartcase_search = true;
-      search = {
-        center_on_match = true;
-        regex = true;
-        case_sensitive = false;
+      agent = {
+        default_model = {
+          provider = "copilot_chat";
+          model = "gpt-4.1";
+        };
+        model_parameters = [ ];
       };
-      prettier = {
-        allowed = false;
-      };
-      lsp_document_colors = "inlay";
-      inlay_hints = {
-        enabled = false;
-        show_background = true;
-      };
-      preferred_line_length = 100;
-      soft_wrap = "prefer_line";
-      toolbar = {
-        agent_review = true;
-      };
-      sticky_scroll = {
-        enabled = true;
-      };
-      autoscroll_on_clicks = true;
-      vertical_scroll_margin = 10.0;
-      scroll_beyond_last_line = "vertical_scroll_margin";
-      rounded_selection = true;
-      current_line_highlight = "all";
-      buffer_line_height = "standard";
       audio = {
         "experimental.auto_microphone_volume" = true;
         "experimental.rodio_audio" = true;
       };
+      autoscroll_on_clicks = true;
+      base_keymap = "VSCode";
+      buffer_font_family = "DejaVu Sans Mono";
+      buffer_font_size = 14.0;
+      buffer_line_height = "standard";
       calls = {
         mute_on_join = true;
       };
-      indent_guides = {
-        active_line_width = 1;
-        background_coloring = "indent_aware";
-        coloring = "indent_aware";
-      };
       colorize_brackets = true;
-      restore_on_startup = "last_session";
+      current_line_highlight = "all";
+      debugger = {
+        dock = "right";
+      };
       diagnostics = {
         inline = {
           padding = 4;
           enabled = true;
         };
       };
-      minimap = {
-        current_line_highlight = "all";
-        thumb_border = "left_open";
-        thumb = "always";
-        show = "auto";
-      };
-      icon_theme = "Zed (Default)";
-      relative_line_numbers = "enabled";
-      features = {
-        edit_prediction_provider = "copilot";
-      };
+      disable_ai = false;
       edit_predictions = {
+        provider = "copilot";
         mode = "subtle";
-      };
-      debugger = {
-        dock = "right";
       };
       git = {
         inline_blame = {
@@ -92,22 +61,34 @@
         };
       };
       helix_mode = true;
-      vim_mode = false;
-      base_keymap = "VSCode";
-      ui_font_family = "DejaVu Sans";
-      buffer_font_family = "DejaVu Sans Mono";
-      redact_private_values = true;
-      agent = {
-        default_model = {
-          provider = "copilot_chat";
-          model = "gpt-4.1";
-        };
-        model_parameters = [ ];
+      icon_theme = "Zed (Default)";
+      indent_guides = {
+        active_line_width = 1;
+        background_coloring = "indent_aware";
+        coloring = "indent_aware";
       };
-      disable_ai = false;
-      ui_font_size = 16.0;
-      buffer_font_size = 14.0;
-      theme = "Zedokai Darker Classic";
+      inlay_hints = {
+        enabled = false;
+        show_background = true;
+      };
+      languages = {
+        Go = {
+          language_servers = [
+            "gopls"
+            "golangci-lint"
+          ];
+        };
+        Rust = { };
+        YAML = {
+          tab_size = 2;
+        };
+        Nix = {
+          language_servers = [
+            "!nil"
+            "nixd"
+          ];
+        };
+      };
       lsp = {
         yaml-language-server = {
           settings = {
@@ -137,30 +118,47 @@
           };
         };
       };
-      languages = {
-        Go = {
-          language_servers = [
-            "gopls"
-            "golangci-lint"
-          ];
-        };
-        Rust = { };
-        YAML = {
-          tab_size = 2;
-        };
-        Nix = {
-          language_servers = [
-            "!nil"
-            "nixd"
-          ];
-        };
+      lsp_document_colors = "inlay";
+      minimap = {
+        current_line_highlight = "all";
+        thumb_border = "left_open";
+        thumb = "always";
+        show = "auto";
       };
+      preferred_line_length = 100;
+      prettier = {
+        allowed = false;
+      };
+      relative_line_numbers = "enabled";
+      redact_private_values = true;
+      restore_on_startup = "last_session";
+      rounded_selection = true;
+      scroll_beyond_last_line = "vertical_scroll_margin";
+      seed_search_query_from_cursor = "selection";
+      search = {
+        center_on_match = true;
+        regex = true;
+        case_sensitive = false;
+      };
+      soft_wrap = "prefer_line";
+      sticky_scroll = {
+        enabled = true;
+      };
+      theme = "Zedokai Darker Classic";
       theme_overrides = {
         "Zedokai Darker Classic" = {
           "terminal.ansi.blue" = "#268bd2";
           "terminal.ansi.bright_blue" = "#268bd2";
         };
       };
+      toolbar = {
+        agent_review = true;
+      };
+      ui_font_family = "DejaVu Sans";
+      ui_font_size = 16.0;
+      use_smartcase_search = true;
+      vertical_scroll_margin = 10.0;
+      vim_mode = false;
     };
   };
 }
