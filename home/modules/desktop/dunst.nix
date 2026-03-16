682c0db253a553b4b21c3fc7edd7aadd21d39984
Josh Robson Chase <josh@robsonchase.com>
2026-03-16 12:02:23 -0400

    prettify dunst a bit

--- home/modules/desktop/dunst.nix
+++ home/modules/desktop/dunst.nix
@@ -3,14 +3,11 @@
   services.dunst.settings = {
     global = {
       font = "Monospace-10";
-      format = "%s\\n%b";
       sort = true;
       indicate_hidden = true;
-      bounce_freq = 0;
       show_age_threshold = 60;
       word_wrap = true;
       ignore_newline = false;
-      geometry = "0x5-30+20";
       transparency = 15;
       idle_threshold = 120;
       monitor = 0;
@@ -18,22 +15,26 @@
       sticky_history = true;
       line_height = 0;
       separator_height = 2;
-      padding = 8;
       horizontal_padding = 8;
       separator_color = "frame";
       startup_notification = false;
       browser = "firefox -new-tab";
-      frame_width = 3;
-      frame_color = "#aaaaaa";
-      timeout = 10;
+      frame_width = 2;
+      frame_color = "#d6d6d6";
+      timeout = 20;
+      corner_radius = 10;
+      origin = "top-center";
+      width = "(0, 500)";
+      height = "(0, 200)";
+      offset = "(0, 20)";
     };
     urgency_low = {
       background = "#222222";
-      foreground = "#ffffff";
+      foreground = "#d6d6d6";
     };
     urgency_normal = {
-      background = "#285577";
-      foreground = "#ffffff";
+      background = "#2e2e2e";
+      foreground = "#d6d6d6";
     };
     urgency_critical = {
       background = "#900000";
