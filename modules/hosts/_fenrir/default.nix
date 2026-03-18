5dacdb6c5672f8d755224ea87c8614cd72c7fed0
Josh Robson Chase <josh@robsonchase.com>
2026-03-18 06:48:17 -0400

    add firefox and xdg-open to satisfy kolide

--- modules/hosts/_fenrir/default.nix
+++ modules/hosts/_fenrir/default.nix
@@ -185,6 +185,8 @@
     helix
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    firefox
+    xdg-utils
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
