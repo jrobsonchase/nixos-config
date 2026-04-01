6c473adec5fbed29db3957185e767c8911cd3344
Josh Robson Chase <josh@robsonchase.com>
2026-04-01 12:00:50 -0400

    switch to podman

--- modules/hosts/_fenrir/default.nix
+++ modules/hosts/_fenrir/default.nix
@@ -112,10 +112,13 @@
     };
   };
 
-  virtualisation.docker = {
-    enable = true;
-    daemon.settings = {
-      ipv6 = false;
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
     };
   };
 
