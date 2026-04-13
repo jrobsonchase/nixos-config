a6c8bffa674608e8858e346ab55a6143723b674b
Josh Robson Chase <josh@robsonchase.com>
2026-04-13 09:51:51 -0400

    add zed cache

--- flake.nix
+++ flake.nix
@@ -48,10 +48,14 @@
     extra-substituters = [
       "https://jrobsonchase.cachix.org"
       "https://nix-community.cachix.org"
+      "https://zed.cachix.org"
+      "https://cache.garnix.io"
     ];
     extra-trusted-public-keys = [
       "jrobsonchase.cachix.org-1:YOmfoK1M7kCwfC/RirSXLck9bplLNFh6U/syn1q/H84="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 }
