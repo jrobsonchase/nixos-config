443aa533448cddc500b67acaef8c6701317b357d
Josh Robson Chase <josh@robsonchase.com>
2026-04-27 14:46:26 -0400

    fenrir: enable tailscale

--- modules/hosts/_fenrir/default.nix
+++ modules/hosts/_fenrir/default.nix
@@ -93,6 +93,8 @@
     "127.0.0.1 registry.local"
   ];
 
+  services.tailscale.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
