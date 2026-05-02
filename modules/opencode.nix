f67e9c7502a24c03073d64ce136296de411be30f
Josh Robson Chase <josh@robsonchase.com>
2026-05-02 13:08:57 -0400

    add opencode module with docker image package

---
+++ modules/opencode.nix
@@ -0,0 +1,17 @@
+{ ... }:
+{
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages = {
+        opencode-image = pkgs.dockerTools.buildLayeredImage {
+          name = "opencode";
+          tag = "latest";
+          contents = [ pkgs.opencode ];
+          config = {
+            Entrypoint = [ "${pkgs.opencode}/bin/opencode" ];
+          };
+        };
+      };
+    };
+}
